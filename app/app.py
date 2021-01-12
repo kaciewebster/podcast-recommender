849a75c1b56838592ce671fe0c107de514e15ba2
kaciewebster <kacieweb98@gmail.com>
2021-01-11 16:11:02 -0800

    finished app

--- app/app.py
+++ app/app.py
@@ -5,41 +5,28 @@ from recommender import PodcastRecommender
 
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+pr = PodcastRecommender()
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 @app.route('/recommend', methods=['POST'])
-def recommendations():
-    user_input = request.json
-    title, num_recs = user_input['title'], user_input['num_recs']
-    rec_list = _recommendations(sim_mat, titles_list, title, num_recs)
-    if rec_list:
-        rec_dict = {letter:rec for letter, rec in zip(['a', 'b', 'c'], rec_list)}
-        return jsonify(rec_dict)
-    else:
-        return jsonify({})
+def recommend():
+    title = str(request.form['title'])
+    num_recs = int(request.form['num_recs'])
 
-def _recommendations(sim_mat, titles_list, title, num_recs):
-    # title = str(request.form['title'])
-    # num_recs = int(request.form['num_recs'])
-    user_input = request.json
-    title, num_recs = user_input['title'], user_input['num_recs']
-
-    if title in titles_list:
-        pr = PodcastRecommender()
-        recommendations = pr.get_recommendations(sim_mat, titles_list, title, num_recs)
-        return recommendations
-    else:
-        return []
+    recommendations = pr.get_recommendations(title, num_recs)
+    return render_template('recommend.html', names=recommendations)
 
 
 
 if __name__ == '__main__':
+    # Load files.
     sim_mat = pd.read_pickle('similarity_matrix.pkl')
-
     with open('titles_list.pkl', 'rb') as f:
         titles_list = pickle.load(f)
+    with open('category_dict.pkl', 'rb') as f:
+        cat_dict = pickle.load(f)
 
     app.run(host='0.0.0.0', port=8080, debug=True)
--- app/recommender.py
+++ app/recommender.py
@@ -4,55 +4,66 @@ import pandas as pd
 import numpy as np
 
 class PodcastRecommender():
-    
+
     def __init__(self):
-        self.sim_mat = None
-        self.titles_list = None
+        # self.sim_mat = None
+        # self.titles_list = None
+        # self.cat_dict = None
+        self.title_recs = None
         self.podcast_recs = None
-        
-    def get_recommendations(self, sim_mat, titles_list, title, num_recs=3):
+
+        self.sim_mat = pd.read_pickle('../app/similarity_matrix.pkl')
+        with open('../app/titles_list.pkl', 'rb') as f:
+            self.titles_list = pickle.load(f)
+        with open('../app/category_dict.pkl', 'rb') as f:
+            self.cat_dict = pickle.load(f)
+
+    def get_recommendations(self, title='So Game We All', num_recs=3):
         '''
         Returns the top num_recs recommendations for the specified title given the similarity matrix of podcasts, a list of the titles, specified title, and the number of recommendations.
         Input:
-            sim_mat: pandas dataframe
-            titles_list: list
             title: string
             num_recs: integer
         Output:
             list
         '''
-        self.sim_mat = sim_mat
-        self.titles_list = titles_list
-        
+        # self.sim_mat = sim_mat
+        # self.titles_list = titles_list
+        # self.cat_dict = cat_dict
         idx_titles = {key:val for key, val in zip(range(0, self.sim_mat.shape[0]), self.titles_list)}
-        key_list = list(idx_titles.keys())
         val_list = list(idx_titles.values())
-        
-        if title in titles_list:
+
+        if title in self.titles_list:
             position = val_list.index(title)
             recs_idx = self.sim_mat.iloc[position].values.argsort()[-(num_recs+1):-1][::-1]
-        
-            self.podcast_recs = []
+
+            self.title_recs = []
             for idx in recs_idx:
-                self.podcast_recs.append(titles_list[idx])
+                self.title_recs.append(self.titles_list[idx])
+
+            self.podcast_recs = {}
+            for title in self.title_recs:
+                self.podcast_recs[title] = self.cat_dict[title]
             return self.podcast_recs
         else:
-            return "Sorry, we couldn't find your podcast."
-    
-    
-    
+            return "We're sorry, but we can't find your podcast!"
+
+
+
 if __name__=='__main__':
     # Load files.
-    sim_mat = pd.read_pickle('../data/similarity_matrix.pkl')
-    with open('../data/titles_list.pkl', 'rb') as f:
+    sim_mat = pd.read_pickle('../app/similarity_matrix.pkl')
+    with open('../app/titles_list.pkl', 'rb') as f:
         titles_list = pickle.load(f)
+    with open('../app/category_dict.pkl', 'rb') as f:
+        cat_dict = pickle.load(f)
+
     # To test.
     sample = random.sample(titles_list, 1)
     sample = sample[0]
-    print('Sample:' + sample)
-    
+    print('Sample:' + f'[{sample}, {cat_dict[sample]}]')
+
     x = PodcastRecommender()
-    recommendations = x.get_recommendations(sim_mat, titles_list, sample)
+    recommendations = x.get_recommendations(sample)
     print(recommendations)
-        
-    
+
